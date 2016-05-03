60246353b27fc2bf6ae95e79ca6dd02627799a32
James Bernsen <jrbtex@gmail.com>
2016-05-03 19:03:59 -0400

    Prepares gh-pages for use with Doxygen.

--- src/ui/lyrDigitime.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void lyrDigitime_create(const GRect position, Layer* lyrParent); 
-  
-void lyrDigitime_destroy();
-
-void lyrDigitime_stylize(const GColor, const GColor, const GTextAlignment, const GFont); 
-
-void lyrDigitime_updateTime();
--- src/ui/wndMainMenu.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <pebble.h>
-
-#include "data/KivaModel.h"
-
-void wndMainMenu_updateView(const KivaModel*);
-void wndMainMenu_createPush(); 
-void wndMainMenu_destroy();
